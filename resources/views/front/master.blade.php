4b8512d1e579a9eef901b1de2f8c31ac682ae951
Md Arman Ali <mdarmanali070@gmail.com>
2024-12-18 16:14:19 +0600

    Update master.blade.php

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -236,9 +236,11 @@
         </li>
 
         <li class="mobile-category">
-            <a href="javascript:void(0)">
-                <i class="iconly-Category icli js-link"></i>
+            <a href="javascript:void(0)" data-bs-toggle="offcanvas" data-bs-target="#primaryMenu">
+                <span class=" navbar-toggler-icon-2">
+                <i data-feather="list" style="color: white"></i>
                 <span>Category</span>
+            </span>
             </a>
         </li>
 
