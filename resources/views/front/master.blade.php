c145de2c9bc147f541506ad789b86761915530e9
Md Arman Ali <mdarmanali070@gmail.com>
2024-12-19 12:24:12 +0600

    dg

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -415,7 +415,7 @@
 
 
             <div class="content-col">
-                <div class="section-b-space">
+                <div class="section-b-space" style="padding-bottom: 0px">
                     <!-- Swiper Container -->
                     <div class="swiper mySwiper">
                         <div class="swiper-wrapper">
@@ -455,7 +455,128 @@
 {{--                        <div class="swiper-button-prev"></div>--}}
                     </div>
                 </div>
+                <!-- Category Section Start -->
+                <section class="section-b-space">
+                    <div class="container">
+                        <div class="title">
+                            <h2>Popular Categories</h2>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="category-slider arrow-slider">
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn">
+                                            <a href="shop-left-sidebar.html" class="circle-1">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/1.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Oils, Refined & Ghee</h6>
+                                            </div>
+                                        </div>
+                                    </div>
 
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.05s">
+                                            <a href="shop-left-sidebar.html" class="circle-2">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/2.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Rice, Flour & Grains</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.1s">
+                                            <a href="shop-left-sidebar.html" class="circle-3">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/3.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Food Cupboard</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.15s">
+                                            <a href="shop-left-sidebar.html" class="circle-4">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/4.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Dals & Pulses</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.2s">
+                                            <a href="shop-left-sidebar.html" class="circle-4">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/5.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Drinks & Beverages</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.25s">
+                                            <a href="shop-left-sidebar.html" class="circle-1">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/6.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Fresh Fruits & Vegetables</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.3s">
+                                            <a href="shop-left-sidebar.html" class="circle-2">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/7.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Ready to eat Meals</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.35s">
+                                            <a href="shop-left-sidebar.html" class="circle-3">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/8.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Instant Mixes</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div class="shop-category-box border-0 wow fadeIn" data-wow-delay="0.4s">
+                                            <a href="shop-left-sidebar.html" class="circle-4">
+                                                <img src="{{asset('/')}}front/assets/images/veg-3/category/2.png" class="img-fluid blur-up lazyload"
+                                                     alt="">
+                                            </a>
+                                            <div class="category-name">
+                                                <h6>Rice, Flour & Grains</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- Category Section End -->
 
 
                 <div class="title d-block">
