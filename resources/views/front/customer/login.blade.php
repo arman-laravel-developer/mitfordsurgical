97aacf89463603c79d44921bffc6c459805313e8
Md Arman Ali <mdarmanali070@gmail.com>
2024-12-24 16:46:11 +0600

    sd

--- resources/views/front/customer/login.blade.php
+++ resources/views/front/customer/login.blade.php
@@ -31,15 +31,14 @@
         <!-- Breadcrumb Section End -->
 
         <!-- log in section start -->
-        <section class="log-in-section background-image-2 section-b-space">
+        <section class="log-in-section background-image-2" style="padding-top: 0">
             <div class="container w-100">
-                <div class="row">
-                    <div class="col-xxl-6 col-xl-5 col-lg-6 d-lg-block d-none ms-auto">
-                        <div class="image-contain">
-                            <img src="{{asset('/')}}front/assets/images/inner-page/log-in.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
+                <div class="row justify-content-center">
+{{--                    <div class="col-xxl-6 col-xl-5 col-lg-6 d-lg-block d-none ms-auto">--}}
+{{--                        <div class="image-contain">--}}
+{{--                            <img src="{{asset('/')}}front/assets/images/inner-page/log-in.png" class="img-fluid" alt="">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                     <div class="col-xxl-4 col-xl-5 col-lg-6 col-sm-8 mx-auto">
                         <div class="log-in-box">
                             <div class="log-in-title">
--- resources/views/front/customer/register.blade.php
+++ resources/views/front/customer/register.blade.php
@@ -7,7 +7,7 @@
 @section('body')
     <div class="content-col">
         <!-- Breadcrumb Section Start -->
-        <section class="breadcrumb-section">
+        <section class="breadcrumb-section" style="padding-top: 0">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
@@ -31,14 +31,14 @@
         <!-- Breadcrumb Section End -->
 
         <!-- log in section start -->
-        <section class="log-in-section section-b-space">
+        <section class="log-in-section background-image-2" style="padding-top: 0">
             <div class="container w-100">
-                <div class="row">
-                    <div class="col-xxl-6 col-xl-5 col-lg-6 d-lg-block d-none ms-auto">
-                        <div class="image-contain">
-                            <img src="{{asset('/')}}front/assets/images/inner-page/sign-up.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
+                <div class="row justify-content-center">
+{{--                    <div class="col-xxl-6 col-xl-5 col-lg-6 d-lg-block d-none ms-auto">--}}
+{{--                        <div class="image-contain">--}}
+{{--                            <img src="{{asset('/')}}front/assets/images/inner-page/sign-up.png" class="img-fluid" alt="">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
 
                     <div class="col-xxl-4 col-xl-5 col-lg-6 col-sm-8 mx-auto">
                         <div class="log-in-box">
@@ -119,8 +119,6 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-xxl-7 col-xl-6 col-lg-6"></div>
                 </div>
             </div>
         </section>
