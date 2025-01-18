9b467dd907d61cf7952c1862e217be77dd23d165
Md Arman Ali <mdarmanali070@gmail.com>
2025-01-18 17:44:10 +0600

    dd

--- resources/views/front/invoice/seller-invoice.blade.php
+++ resources/views/front/invoice/seller-invoice.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Invoice</title>
+    <title>{{route('home')}}-{{$order->order_code}}-Invoice</title>
     <style>
         body {
             margin: 0;
